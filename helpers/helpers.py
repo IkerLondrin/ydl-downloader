6b8f728dc693ef1bb7d54b6749e3ce1ba1b2f8bf
Iker Londrin <46924838+IkerLondrin@users.noreply.github.com>
2021-08-12 20:22:54 +0200

    Update helpers.py

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -2,23 +2,8 @@ import youtube_dl
 import requests
 import sys
 import urllib.parse as urlparse
-# import sqlite3
 import os
 from flaskext.mysql import MySQL
-# from prettytable import PrettyTable
-# from sqlite3 import Error
-# import requests
-# from bs4 import BeautifulSoup
-
-def ph_url_check(url):
-    parsed = urlparse.urlparse(url)
-    regions = ["www", "cn", "cz", "de", "es", "fr", "it", "nl", "jp", "pt", "pl", "rt"]
-    for region in regions:
-        if parsed.netloc == region + ".pornhub.com":
-            print(" Url de PH validad...")
-            return
-    print(" Error, no es un video de PH.")
-    sys.exit()
 
 def check_login(username, password):
     # cur = mysql.connection.cursor()
